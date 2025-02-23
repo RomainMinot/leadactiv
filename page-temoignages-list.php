c7d73b57363085414bbca2153e234e375f4e2faf
Romain Minot <rom_ino@hotmail.fr>
2025-02-23 02:47:42 +0100

    Adaptation des filtres - Page annuaire de témoignages
    Layout

--- page-temoignages-list.php
+++ page-temoignages-list.php
@@ -38,19 +38,75 @@
     <!-- Témoignages list -->
     <section class="page__temoignages__list bg-white position-relative">
         <div class="container__lg">
-            <div class="py-5 py-md-8">
-            <div class="row g-4">
-                <?php
-                if ($case_studies && !empty($case_studies)) {
-                    foreach ($case_studies as $index => $case_study) {
-                        $case_study_fields = get_fields($case_study->ID);
-                        include(get_template_directory() . '/template-parts/card-etude-cas-component.php'); 
+            <div class="py-5 pt-2 pb-md-8">
+                <!-- Filter -->
+                <div class="py-4 mb-2">
+                    <div class="row d-flex align-items-stretch justify-content-between">
+                        <div class="col-8">
+                            <p class="w-100 mb-3">Filtre</p>
+                            <div class="w-100 d-flex align-items-center justify-content-center flex-nowrap gap-2">
+                                <div class="flex-1">
+                                    <label for="selectSector" class="label">Secteur</label>
+                                    <select id="selectSector" class="input" name="sector">
+                                        <option value="direction_marketing">Direction marketing</option>
+                                    </select>
+                                </div>
+                                <div class="flex-1">
+                                    <label for="selectFonction" class="label">Fonction ciblée</label>
+                                    <select id="selectFonction" class="input" name="fonction">
+                                        <option value="assistant_de_direction">Assistant(e) de direction</option>
+                                    </select>
+                                </div>
+                                <div class="flex-1">
+                                    <label for="selectLocalisation" class="label">Localisation</label>
+                                    <select id="selectLocalisation" class="input" name="localisation">
+                                        <option value="france">France</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-3 d-flex flex-column align-items-start justify-content-between">
+                            <p class="w-100 mb-3">Recherche</p>
+                            <input type="text" class="input input--search" placeholder="Search" name="search">
+                        </div>
+                    </div>
+                    <div class="row my-3">
+                        <div class="col-12">
+                            <div class="d-flex flex-row align-items-center justify-content-start flex-wrap gap-2">
+                                <div class="tag tag-light-purple">
+                                    <span>Alimentation & Hôtellerie</span>
+                                    <i class="ml-2 fas fa-times"></i>
+                                </div>
+                                <div class="tag tag-gray">
+                                    <span>Direction marketing</span>
+                                    <i class="ml-2 fas fa-times"></i>
+                                </div>
+                                <div class="tag tag-gray">
+                                    <span>Assistant(e) de direction</span>
+                                    <i class="ml-2 fas fa-times"></i>
+                                </div>
+                                <div class="tag tag-gray">
+                                    <span>France</span>
+                                    <i class="ml-2 fas fa-times"></i>
+                                </div>
+                                <button class="page__temoignages__list__filters__clear">Clear filters <i class="ml-1 fas fa-times"></i></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- List -->
+                <div class="row g-4">
+                    <?php
+                    if ($case_studies && !empty($case_studies)) {
+                        foreach ($case_studies as $index => $case_study) {
+                            $case_study_fields = get_fields($case_study->ID);
+                            include(get_template_directory() . '/template-parts/card-etude-cas-component.php'); 
+                        }
+                    } else {
+                        echo '<p>Aucune etude de cas trouvée.</p>';
                     }
-                } else {
-                    echo '<p>Aucune etude de cas trouvée.</p>';
-                }
-                ?>
-            </div>
+                    ?>
+                </div>
             </div>
         </div>
     </section>
